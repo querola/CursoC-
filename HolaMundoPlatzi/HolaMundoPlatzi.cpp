a927ed31f93fa37e5133ff0f0870e49a4bd59a6f
Rodrigo-PcGamer <el_stroke_666@hotmail.com>
2020-05-08 16:35:28 -0500

    Agregar archivos de proyecto.

---
+++ HolaMundoPlatzi/HolaMundoPlatzi.cpp
@@ -0,0 +1,22 @@
+// HolaMundoPlatzi.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   
+    int Direccion = 3509;
+    cout << Direccion << endl;
+    //crear un apuntador
+    int* ApuntadorDir;
+
+    ApuntadorDir = &Direccion;
+    //se asigna 3 a la direccion de memoria que marca el apuntador
+    *ApuntadorDir = 3;
+    cout << Direccion << endl;
+    
+    return 0;
+}
+
+
